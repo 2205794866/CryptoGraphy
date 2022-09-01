8f788d13df4c7e7f2742161634259f15fbf3bd4c
IcathianRain <2205794866@qq.com>
2022-09-01 19:34:53 +0800

    RSA

---
+++ src/5_RSA/RSA.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+// delete it if you submit the answer
+#define LOCAL
+
+int main()
+{
+    #ifdef LOCAL
+    freopen("test/1.in", "r", stdin);
+    #endif
+    // Your code here
+
+
+
+    
+
+    return 0;
+}
